d1167f585b63835c8697101fcb8ea913f9982abf
pfefferle <pfefferle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-06 20:09:27 +0000

    Version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/activitystream-extension/trunk@108228 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ activity-extension.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Plugin Name: ActivityStream extension
+Plugin URI: 
+Description: An extensions which adds the ActivityStream (<a href="http://www.activitystrea.ms">activitystrea.ms</a>) syntax to your Atom-Feed
+Author: Matthias Pfefferle
+Version: 0.1
+Author URI: http://notizblog.org
+*/
+
+if (isset($wp_version)) {
+  add_action('atom_ns', array('ActivityExtension', 'addActivityNamespace'));
+  add_action('atom_entry', array('ActivityExtension', 'addActivityObject'));
+  add_action('comment_atom_ns', array('ActivityExtension', 'addActivityNamespace'));
+  add_action('comment_atom_entry', array('ActivityExtension', 'addCommentActivityObject'));
+}
+
+/**
+ *
+ */
+class ActivityExtension {
+  
+  /**
+   * echos the activitystream namespace
+   */
+  function addActivityNamespace() {
+    echo 'xmlns:activity="http://activitystrea.ms/schema/1.0/"';
+  }
+
+  /**
+   * echos the activity verb and object for the wordpress entries
+   */
+  function addActivityObject() {
+?>
+    <activity:verb>http://activitystrea.ms/schema/1.0/post/</activity:verb>
+    <activity:object>
+      <activity:object-type>http://activitystrea.ms/schema/1.0/blog-entry/</activity:object-type>
+      <activity:object-type>http://activitystrea.ms/schema/1.0/article/</activity:object-type>
+      <id>id:activity:post:<?php the_id(); ?></id>
+      <title type="<?php html_type_rss(); ?>"><![CDATA[<?php the_author() ?> posted a new blog-entry]]></title>
+      <link rel="alternate" type="text/html" href="<?php the_permalink_rss() ?>" />
+    </activity:object>
+<?php
+  }
+  
+  /**
+   * echos the activity verb and object for the wordpress comments
+   */
+  function addCommentActivityObject() {
+?>
+    <activity:verb>http://activitystrea.ms/schema/1.0/post/</activity:verb>
+    <activity:object>
+      <id>id:activity:comment:<?php comment_id(); ?></id>
+      <title type="<?php html_type_rss(); ?>"><![CDATA[<?php comment_author_rss() ?> posted a comment]]></title>
+      <link rel="alternate" type="text/html" href="<?php comment_link() ?>" />
+      <thr:in-reply-to ref="<?php the_guid() ?>" href="<?php the_permalink_rss() ?>" type="<?php bloginfo_rss('html_type'); ?>" />
+    </activity:object>
+<?php
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,18 @@
+=== Plugin Name ===
+Contributors: pfefferle
+Donate link: http://notizblog.org
+Tags: Activities, Activity Stream, Feed, RSS, Atom
+Requires at least: 2.5
+Tested up to: 2.7.1
+Stable tag: 0.1
+
+An extensions which adds the ActivityStream ([activitystrea.ms](http://www.activitystrea.ms)) syntax to your Atom-Feed
+
+You can find a demo file here: [notizBlog.org/apml/](http://notizblog.org/apml/).
+
+== Installation ==
+
+* Upload the whole folder to your `wp-content/plugins` folder
+* Activate it at the admin interface
+
+Thats it
